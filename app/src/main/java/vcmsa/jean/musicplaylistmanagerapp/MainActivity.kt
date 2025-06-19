898af5dd7b43155f830a811228d724a8e3459713
Jean <ST10478055@vcconnect.edu.za>
2025-06-19 10:27:52 +0200

    Arrays Commit

--- app/src/main/java/vcmsa/jean/musicplaylistmanagerapp/MainActivity.kt
+++ app/src/main/java/vcmsa/jean/musicplaylistmanagerapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,18 +19,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }//code starts here
 
+        val songTitles = arrayListOf<String>()
+        val artistName = arrayListOf<String>()
+        val rating = arrayListOf<Int>()
+        val comments = arrayListOf<String>()
+
         val btnAddToPlaylist = findViewById<Button>(R.id.btnAddToPlaylist)
+        val edtSongTitles = findViewById<EditText>(R.id.edtSongTitles)
+        val edtArtistName = findViewById<EditText>(R.id.edtArtistName)
+        val edtRating = findViewById<EditText>(R.id.edtRating)
+        val edtComments = findViewById<EditText>(R.id.edtComments)
         val btnNextScreen = findViewById<Button>(R.id.btnNextScreen)
         val btnExitApp = findViewById<Button>(R.id.btnExitApp)
-        val edtEnterDetailsHere = findViewById<EditText>(R.id.edtEnterDetailsHere)
-        val tvEnterDetails = findViewById<TextView>(R.id.tvEnterDetails)
-
-        btnAddToPlaylist.setOnClickListener {
-            tvEnterDetails.text = "Add Playlist Details Below"
-
         }
 
-    }
-}
+
+            }
+
